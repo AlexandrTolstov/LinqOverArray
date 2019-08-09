84676abb9a29edbeb54cd6ce42c310c88f32067a
Толстов Александр <alextav_85@mail.ru>
2019-08-09 10:15:49 +0600

    Add project files.

---
+++ LinqOverArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqOverArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Fun with LINQ to Object *****\n");
+            QueryOverString();
+
+        }
+        static void QueryOverString()
+        {
+            string[] currentVideoGames = {"Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2"};
+            IEnumerable<string> subset = from g in currentVideoGames where g.Contains(" ") orderby g select g;
+            foreach(string s in subset)
+                Console.WriteLine("Item: {0}", s);
+        }
+    }
+}
